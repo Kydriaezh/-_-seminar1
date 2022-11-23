d9e63bff9fbc15ef1623ef76b069cf3965e107b3
Kydriaezh <bazanova.nata@gmail.com>
2022-11-24 02:41:56 +0300

    Task3

---
+++ Lesson1/1_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая будет выдаваьб название для недели по заданному номеру.
+Console.WriteLine("ВВедите число");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num1 == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (num1 == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (num1 == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (num1 == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (num1 == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else
+{
+    Console.WriteLine("error");
+}
