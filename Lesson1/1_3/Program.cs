c55d4f30faf7b7c514df3aa35a7e6e40bf0017ca
Kydriaezh <bazanova.nata@gmail.com>
2022-11-24 03:18:35 +0300

    Task 4

---
+++ Lesson1/1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает одно число, а на выходе показывает все целые числа в промежутке от -N lj N
+Console.WriteLine("Введите число");
+string N_s = Console.ReadLine();
+int N = int.Parse(N_s);
+int OtrN = -N;
+
+while(OtrN <= N)
+{
+    Console.Write($"{OtrN}");
+    OtrN +=1;
+}
