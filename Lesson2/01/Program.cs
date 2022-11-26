3b4e9a35110c32d2de08da9e9f976ca21899beb8
Kydriaezh <bazanova.nata@gmail.com>
2022-11-27 00:48:02 +0300

    Task 01

---
+++ Lesson2/01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99 и покзывает наибольшую цифру
+
+
+int Maxnumber(int num)
+{
+    Console.WriteLine(num);
+
+    if (num % 10 > num / 10)
+        return num % 10;
+    else
+        return num / 10;
+}
+Console.WriteLine(Maxnumber(new Random().Next(10,100)));
+
+
