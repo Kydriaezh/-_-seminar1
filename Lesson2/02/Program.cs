e740501ce0daacfb69a60e07e021219fb2bc2973
Kydriaezh <bazanova.nata@gmail.com>
2022-11-27 01:45:48 +0300

    Task2 add another way

--- Lesson2/02/Program.cs
+++ Lesson2/02/Program.cs
@@ -2,11 +2,12 @@
 // и на выходе показывает последнюю цифру этого числа.
 
 
-int Maxnumber(int num)
-{
+//int Maxnumber(int num)
+//{
   
-   return num % 10;
-}
-Console.WriteLine(Maxnumber(new Random().Next(100, 1000)));
+//   return num % 10;
+//}
+//Console.WriteLine(Maxnumber(new Random().Next(100, 1000)));
 
-
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num % 10);
