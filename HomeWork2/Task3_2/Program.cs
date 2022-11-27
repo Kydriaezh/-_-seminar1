8f3a45c36d1f4281659a385e7373c11937fcc592
Kydriaezh <bazanova.nata@gmail.com>
2022-11-28 00:51:36 +0300

    Task 1.2.4

---
+++ HomeWork2/Task3_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+
+    if (num < 100)   
+    Console.WriteLine("no");    
+        {
+            while (num > 999)
+            { 
+                num = num / 10; 
+            }
+            Console.WriteLine(num % 10); 
+        }
+       
+    
+   
+
+
+
+
+// while (num > 999)
+ { 
+    num = num / 10; 
+ } 
+//ьConsole.WriteLine(Xz(num % 10));      
+
+ 
+ 
+ 
+ 
+ 
+
+
