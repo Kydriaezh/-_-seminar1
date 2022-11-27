0c6e89766358c7ba3f9870880c125841fe06ceb1
Kydriaezh <bazanova.nata@gmail.com>
2022-11-28 00:50:44 +0300

    Task4

--- HomeWork/Task4/Program.cs
+++ HomeWork/Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число N, а на выходе
+// показывет все чётные числа от 1 до N
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int i = 2;
+while (i <= num)
+    {
+        Console.WriteLine($"{i}");
+        i += 2;
+    }
