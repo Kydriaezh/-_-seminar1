3992638fc1b39ff8f59dc0d34874050b106861e4
Kydriaezh <bazanova.nata@gmail.com>
2022-11-27 03:05:15 +0300

    Task 4 and 4_02

---
+++ Lesson2/04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу,которая принимает на вход число и 
+// проверяет, кратно ли оно одновременно 7 и 23
+
+Console.WriteLine("Write number 1");
+int first = int.Parse(Console.ReadLine());
+
+string Crat (int num = 7, int num2 = 23)
+{
+    if (first % num == 0 && first % num2 == 0)   
+        return "kratno";
+    else    
+        return "nekratno";
+}
+Console.WriteLine (Crat(first));
+
---
+++ Lesson2/04_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write number 1");
+int first = int.Parse(Console.ReadLine());
+
+string Crat (int num)
+{
+    if (num % 23 == 0 && num % 7 == 0)   
+        return "kratno";
+    else    
+        return "nekratno";
+}
+Console.WriteLine (Crat(first));
