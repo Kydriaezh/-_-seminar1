0787bdab60f268f890e63e5ce578207a28f09021
Kydriaezh <bazanova.nata@gmail.com>
2022-11-28 01:42:04 +0300

    pravki

--- HomeWork/Task1/Program.cs
+++ HomeWork/Task1/Program.cs
@@ -7,7 +7,6 @@ int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("ВВедите число 2");
 int num2 = int.Parse(Console.ReadLine());
 
-
 if (num1 > num2)
 {
    Console.Write($"Число {num1} больше чем {num2}");
