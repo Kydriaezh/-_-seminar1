72bc998802cca54bd62932be92ebdd5e8e91c06f
Kydriaezh <bazanova.nata@gmail.com>
2022-12-02 01:24:35 +0300

    Zadacha 1

---
+++ Lesson3/1_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Напишите программу которая впринимает на вход координаты точки Х У,
+void Qtrs (int x, int y)
+{
+    if (x == 0 || y == 0)
+    Console.WriteLine("x=0 or y=0");
+    else if (x > 0 && y > 0)
+    Console.WriteLine("I");
+    else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+    Console.WriteLine("IV");
+}
+Qtrs(7,8);
