4e2fcb1d7fc0448d0c484717162cf92ab80cc7ad
Kydriaezh <bazanova.nata@gmail.com>
2022-12-21 09:52:16 +0300

    Lesson 7

---
+++ HomeWork6/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданными уровнениями
+// y=k1*x +b1, y=k28x+b2, значения b1, k1, b2, k2 здаются пользователем
+
+void Tp (double b1, double b2, double k1, double k2)
+{
+     
+    double x = (b2-b1)/(k1-k2);
+    double y = b1*x + k1;
+    if (x != 0 && y != 0);
+    Console.WriteLine(x,y);
+}
+ 
+Console.WriteLine(Tp(int.Parse(Console.ReadLine())));
--- Lesson6/6_3/Program.cs
+++ Lesson6/6_3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// выведите первые N чисел Фибоначчи.
+
+void Fibb (int num)
+{
+    int a = 0, b = 1;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"{a}");
+        (a,b)=(b, a+b);
+    }
+}
+int num = int.Parse(Console.ReadLine());
+Fibb(num);
