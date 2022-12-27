1000b5c19b50c9c943da161a8ffa503ebcf38155
Kydriaezh <bazanova.nata@gmail.com>
2022-12-28 01:59:36 +0300

    homework6

--- HomeWork6/task2/Program.cs
+++ HomeWork6/task2/Program.cs
@@ -2,12 +2,19 @@
 // y=k1*x +b1, y=k28x+b2, значения b1, k1, b2, k2 здаются пользователем
 
 void Tp (double b1, double b2, double k1, double k2)
-{
-     
+
+{   
+    if (x != 0 && y != 0) 
+    {    
     double x = (b2-b1)/(k1-k2);
-    double y = b1*x + k1;
-    if (x != 0 && y != 0);
-    Console.WriteLine(x,y);
+    double y = b1*x + k1;    
+    Console.WriteLine($" точка пересечения {x},{y}");
+    }
+    else
+    Console.Write("нет точек пересечения");
 }
- 
-Console.WriteLine(Tp(int.Parse(Console.ReadLine())));
+
+Tp(int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()));
---
+++ HomeWork9/tsk1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int Num (int n, int m)
+{
+    if (n > m)
+    
+
+}
